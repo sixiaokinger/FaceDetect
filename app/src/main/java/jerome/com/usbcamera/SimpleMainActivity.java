20a152b7c1356093f87ad868b218562dc119991e
yanbo <1360695995@qq.com>
2018-03-23 21:12:09 +0800

    add card regisiter

--- app/src/main/java/jerome/com/usbcamera/SimpleMainActivity.java
+++ app/src/main/java/jerome/com/usbcamera/SimpleMainActivity.java
@@ -4,24 +4,35 @@ import android.Manifest;
 import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
 import android.content.ComponentName;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.arcsoft.facedetection.AFD_FSDKEngine;
+import com.arcsoft.facedetection.AFD_FSDKError;
+import com.arcsoft.facedetection.AFD_FSDKFace;
+import com.arcsoft.facedetection.AFD_FSDKVersion;
 import com.arcsoft.facerecognition.AFR_FSDKEngine;
 import com.arcsoft.facerecognition.AFR_FSDKError;
 import com.arcsoft.facerecognition.AFR_FSDKFace;
@@ -32,6 +43,7 @@ import com.arcsoft.facetracking.AFT_FSDKError;
 import com.arcsoft.facetracking.AFT_FSDKFace;
 import com.arcsoft.facetracking.AFT_FSDKVersion;
 import com.dk.bleNfc.Tool.StringTool;
+import com.guo.android_extend.image.ImageConverter;
 import com.guo.android_extend.java.AbsLoop;
 
 import java.util.ArrayList;
@@ -54,7 +66,7 @@ import jerome.com.bleNfc.card.Iso14443bCard;
  * Created by yanbo on 2018/3/22.
  */
 
-public class SimpleMainActivity extends Activity implements FaceDetectView.OnCameraListener, View.OnClickListener {
+public class SimpleMainActivity extends Activity implements FaceDetectView.OnPictureSaved, FaceDetectView.OnCameraListener, View.OnClickListener {
 
     final private String TAG = this.getClass().toString();
     private final static int MSG_REFREASH_TEXT = 0x0000;
@@ -64,6 +76,7 @@ public class SimpleMainActivity extends Activity implements FaceDetectView.OnCam
     private final static int MSG_EVENT_START = 0x1003;
     private final static int MSG_NFC_CONNECTED = 0x2000;
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    private static final int REQUEST_CODE_IMAGE_OP = 2;
 
     private UIHandler mUIHandler = new UIHandler();
     private FRAbsLoop mFRAbsLoop = new FRAbsLoop();
@@ -71,7 +84,10 @@ public class SimpleMainActivity extends Activity implements FaceDetectView.OnCam
     private FaceDetectView mUsbCamera;
     private ImageView mImgCertificate;
     private ImageView mImgMatched;
+
     private ImageButton mBtnRegister;
+    private ImageButton mBtnRegFace;
+    private ImageButton mBtnRegCard;
     private TextView mTextInfo;
     StringBuffer debugInfo = new StringBuffer();
 
@@ -79,12 +95,15 @@ public class SimpleMainActivity extends Activity implements FaceDetectView.OnCam
     private AFT_FSDKEngine tEngine = new AFT_FSDKEngine();
     private List<AFT_FSDKFace> tFaces = new ArrayList<AFT_FSDKFace>();
 
+    private Bitmap mBitmap = null;
     private byte[] mImageNV21 = null;
     private AFR_FSDKFace rFace = null;
     private AFT_FSDKFace tFace = null;
 
     private boolean mGetCertificate = false;
     private boolean mMatched = false;
+
+    private String mCardId = null;
 
     private BleNfcDevice mBleNfcDevice;
     private jerome.com.bleNfc.BleManager.Scanner mScanner;
@@ -100,14 +119,23 @@ public class SimpleMainActivity extends Activity implements FaceDetectView.OnCam
         this.setContentView(R.layout.simple_main);
         mUsbCamera = (FaceDetectView) findViewById(R.id.detect_view);
         mUsbCamera.setCameraListener(this);
+        mUsbCamera.setSavedCallback(this);
 
         mImgCertificate = (ImageView) findViewById(R.id.img_certificate);
         mImgCertificate.setVisibility(View.INVISIBLE);
         mImgMatched = (ImageView) findViewById(R.id.img_matched);
         mImgMatched.setVisibility(View.INVISIBLE);
+
         mBtnRegister = (ImageButton) findViewById(R.id.btn_register);
         mBtnRegister.setOnClickListener(this);
         mBtnRegister.setVisibility(View.VISIBLE);
+        mBtnRegFace = (ImageButton) findViewById(R.id.btn_reg_face);
+        mBtnRegFace.setOnClickListener(this);
+        mBtnRegFace.setVisibility(View.VISIBLE);
+        mBtnRegCard = (ImageButton) findViewById(R.id.btn_reg_card);
+        mBtnRegCard.setOnClickListener(this);
+        mBtnRegCard.setVisibility(View.VISIBLE);
+
         mTextInfo = (TextView) findViewById(R.id.text_info);
 
         AFT_FSDKError tError = tEngine.AFT_FSDK_InitialFaceEngine(FaceDB.appid, FaceDB.ft_key, AFT_FSDKEngine.AFT_OPF_0_HIGHER_EXT, 16, 5);
@@ -154,6 +182,131 @@ public class SimpleMainActivity extends Activity implements FaceDetectView.OnCam
         super.onDestroy();
 
         unbindService(mServiceConnection);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_IMAGE_OP && resultCode == RESULT_OK) {
+            Uri mPath = data.getData();
+            String file = getPath(mPath);
+            Bitmap bmp = Application.decodeImage(file);
+            mImgCertificate.setImageBitmap(bmp);
+        }
+    }
+
+    private String getPath(Uri uri) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (DocumentsContract.isDocumentUri(this, uri)) {
+                // ExternalStorageProvider
+                if (isExternalStorageDocument(uri)) {
+                    final String docId = DocumentsContract.getDocumentId(uri);
+                    final String[] split = docId.split(":");
+                    final String type = split[0];
+
+                    if ("primary".equalsIgnoreCase(type)) {
+                        return Environment.getExternalStorageDirectory() + "/" + split[1];
+                    }
+
+                    // TODO handle non-primary volumes
+                } else if (isDownloadsDocument(uri)) {
+
+                    final String id = DocumentsContract.getDocumentId(uri);
+                    final Uri contentUri = ContentUris.withAppendedId(
+                            Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                    return getDataColumn(this, contentUri, null, null);
+                } else if (isMediaDocument(uri)) {
+                    final String docId = DocumentsContract.getDocumentId(uri);
+                    final String[] split = docId.split(":");
+                    final String type = split[0];
+
+                    Uri contentUri = null;
+                    if ("image".equals(type)) {
+                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("video".equals(type)) {
+                        contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("audio".equals(type)) {
+                        contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                    }
+
+                    final String selection = "_id=?";
+                    final String[] selectionArgs = new String[] {
+                            split[1]
+                    };
+
+                    return getDataColumn(this, contentUri, selection, selectionArgs);
+                }
+            }
+        }
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor actualimagecursor = this.getContentResolver().query(uri, proj, null, null, null);
+        int actual_image_column_index = actualimagecursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        actualimagecursor.moveToFirst();
+        String img_path = actualimagecursor.getString(actual_image_column_index);
+        String end = img_path.substring(img_path.length() - 4);
+        if (0 != end.compareToIgnoreCase(".jpg") && 0 != end.compareToIgnoreCase(".png")) {
+            return null;
+        }
+        return img_path;
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context The context.
+     * @param uri The Uri to query.
+     * @param selection (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
     }
 
     private final ServiceConnection mServiceConnection = new ServiceConnection() {
@@ -347,8 +500,19 @@ public class SimpleMainActivity extends Activity implements FaceDetectView.OnCam
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btn_register) {
-            //todo: enter register activity
+        switch (v.getId()) {
+            case R.id.btn_register:
+                // TODO: 2018/3/23 save info
+                break;
+            case R.id.btn_reg_face:
+                mUsbCamera.capturePicture();
+                break;
+            case R.id.btn_reg_card:
+                Intent getImageByalbum = new Intent(Intent.ACTION_GET_CONTENT);
+                getImageByalbum.addCategory(Intent.CATEGORY_OPENABLE);
+                getImageByalbum.setType("image/jpeg");
+                startActivityForResult(getImageByalbum, REQUEST_CODE_IMAGE_OP);
+                break;
         }
     }
 
@@ -448,13 +612,14 @@ public class SimpleMainActivity extends Activity implements FaceDetectView.OnCam
                             {0x00, (byte) 0x84, 0x00, 0x00, 0x08},
                             {0x00, 0x36, 0x00, 0x00, 0x08},
                     };
+                    int count = 0;
                     debugInfo.append("寻到ISO14443-B卡->UID:(身份证发送0036000008指令获取UID)\r\n");
                     for (byte[] aBytes : sfzCmdBytes) {
                         try {
                             byte returnBytes[] = iso14443bCard.transceive(aBytes);
-
-                            debugInfo.append("读取卡片信息 " + StringTool.byteHexToSting(returnBytes) + "\r\n");
-                            mUIHandler.sendEmptyMessage(MSG_REFREASH_TEXT);
+                            if (count++ > 0) {
+                                onCardInfoCatch(StringTool.byteHexToSting(returnBytes));
+                            }
                         } catch (CardNoResponseException e) {
                             e.printStackTrace();
                             return false;
@@ -464,6 +629,73 @@ public class SimpleMainActivity extends Activity implements FaceDetectView.OnCam
                 break;
         }
         return false;
+    }
+
+    private void onCardInfoCatch(String uid) {
+        debugInfo.append("读取卡片信息 " + uid + "\r\n");
+        mUIHandler.sendEmptyMessage(MSG_REFREASH_TEXT);
+
+        mCardId = uid;
+        mGetCertificate = true;
+    }
+
+    @Override
+    public void onPictureSaved(Bitmap bmp) {
+        mBitmap = bmp;
+        Rect src = new Rect();
+        Rect dst = new Rect();
+
+        src.set(0, 0, bmp.getWidth(), bmp.getHeight());
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                byte[] data = new byte[mBitmap.getWidth() * mBitmap.getHeight() * 3 / 2];
+                ImageConverter converter = new ImageConverter();
+                converter.initial(mBitmap.getWidth(), mBitmap.getHeight(), ImageConverter.CP_PAF_NV21);
+                if (converter.convert(mBitmap, data)) {
+                    Log.d(TAG, "run: convert ok");
+                }
+                converter.destroy();
+
+                AFD_FSDKEngine dEngine = new AFD_FSDKEngine();
+                AFD_FSDKVersion dVersion = new AFD_FSDKVersion();
+                List<AFD_FSDKFace> dFaces = new ArrayList<AFD_FSDKFace>();
+                AFD_FSDKError dError = dEngine.AFD_FSDK_InitialFaceEngine(FaceDB.appid, FaceDB.fd_key, AFD_FSDKEngine.AFD_OPF_0_HIGHER_EXT, 16, 5);
+                Log.e(TAG, "AFD_FSDK_InitialFaceEngine = " + dError.getCode());
+                dError = dEngine.AFD_FSDK_GetVersion(dVersion);
+                Log.e(TAG, "AFD_FSDK_GetVersion = " + dError.getCode());
+                dError = dEngine.AFD_FSDK_StillImageFaceDetection(data, mBitmap.getWidth(), mBitmap.getHeight(), AFD_FSDKEngine.CP_PAF_NV21, dFaces);
+                Log.e(TAG, "AFD_FSDK_StillImageFaceDetection = " + dError.getCode() + "<" + dFaces.size());
+
+                if (!dFaces.isEmpty()) {
+                    AFR_FSDKVersion rVersion = new AFR_FSDKVersion();
+                    AFR_FSDKEngine rEngine = new AFR_FSDKEngine();
+                    AFR_FSDKError rError = rEngine.AFR_FSDK_InitialEngine(FaceDB.appid, FaceDB.fr_key);
+                    AFR_FSDKFace result = new AFR_FSDKFace();
+                    Log.e(TAG, "onPreview: AFR_FSDK_InitialEngine = " + rError.getCode());
+                    rError = rEngine.AFR_FSDK_GetVersion(rVersion);
+                    Log.e(TAG, "onPreview: AFR_FSDK_GetVersion = " + rVersion.getFeatureLevel());
+                    rError = rEngine.AFR_FSDK_ExtractFRFeature(data, mBitmap.getWidth(), mBitmap.getHeight(), AFR_FSDKEngine.CP_PAF_NV21, new Rect(dFaces.get(0).getRect()), dFaces.get(0).getDegree(), result);
+                    rFace = result.clone();
+                    Log.e(TAG, "onPreview: AFR_FSDK_ExtractFRFeature = " + rError.getCode());
+                    rError = rEngine.AFR_FSDK_UninitialEngine();
+                    Log.e(TAG, "onPreview: AFR_FSDK_UninitialEngine = " + rError.getCode());
+
+                    AFD_FSDKFace face = dFaces.get(0);
+                    int width = face.getRect().width();
+                    int height = face.getRect().height();
+                    Bitmap face_bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+                    Canvas face_canvas = new Canvas(face_bitmap);
+                    face_canvas.drawBitmap(mBitmap, face.getRect(), new Rect(0, 0, width, height), null);
+                    Message reg = Message.obtain();
+                    reg.what = MSG_CODE;
+                    reg.arg1 = MSG_EVENT_REG;
+                    reg.obj = face_bitmap;
+                    mUIHandler.sendMessage(reg);
+                }
+            }
+        });
+        thread.start();
     }
 
     class UIHandler extends Handler {
